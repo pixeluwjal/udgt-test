751045362ca7cf63d8e07c384d8eeffe4588556e
sumangalasn <sumangalaace@gmail.com>
2025-07-27 20:39:11 +0530

    Configure Next.js (via next.config.ts) to ignore TypeScript build errors

--- Documents/job/yugt/next.config.ts
+++ Documents/job/yugt/next.config.ts
@@ -5,7 +5,13 @@ const nextConfig = {
   // images: {
   //   domains: ['example.com'],
   // },
-
+   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
